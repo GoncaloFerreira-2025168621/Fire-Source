34fbaa9f76774f0ec48e6f997c791dae0e0d2626
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-04-26 11:01:49 +0100

    Update Tochas.cs

--- Fire Source/Assets/Scripts/Tochas.cs
+++ Fire Source/Assets/Scripts/Tochas.cs
@@ -1,7 +1,11 @@
+using System;
 using UnityEngine;
 
 public class Tochas : MonoBehaviour
 {
+    public GameObject _Fogo;
+    //public GameObject tochas;
+    public string _TagPlayer = "Player";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,5 +17,14 @@ public class Tochas : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(_TagPlayer))
+        {
+            Debug.Log("Tocha acesa");
+            _Fogo.SetActive(true);
+        }
     }
 }
