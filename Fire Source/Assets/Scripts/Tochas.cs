f0479d6761bee1097dc5efe8149136fe0a7470c4
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-04-27 18:28:40 +0100

    Squashed commit of the following:
    commit 2a0f4a4d4fd57ceca9d5abef8e962ed8fd8be490
    Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
    Date:   Mon Apr 27 18:26:51 2026 +0100
    
        New Scene
    
    commit ed143a0b95ad825b4321375b19b24386537a0f51
    Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
    Date:   Mon Apr 27 17:16:07 2026 +0100
    
        Squashed commit of the following:
    
        commit 6c589664f704eb69be37ec285b26389b6bcc63ba
        Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
        Date:   Mon Apr 27 17:14:42 2026 +0100
    
            Light com aumento progressivo
    
        commit a1d639043368acad5f0e4cdd74d047e6b8d3ea1a
        Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
        Date:   Sun Apr 26 20:06:57 2026 +0100
    
            Light Atualizada
    
        commit 8e8f4e2021018d8ad8d48d604580199d178ccce9
        Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
        Date:   Sun Apr 26 13:27:54 2026 +0100
    
            Scale Fire and light
    
        commit 7ca66b007701225690793f5745938556f06a626d
        Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
        Date:   Sun Apr 26 11:27:47 2026 +0100
    
            Nevoa e Luz
    
    commit 8ba6e982fc53b849cfe4e03da6b51409b5a5711a
    Author: GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
    Date:   Mon Apr 27 17:15:52 2026 +0100
    
        Create Fire Force.pdf.docx

--- Fire Source/Assets/Scripts/Tochas.cs
+++ Fire Source/Assets/Scripts/Tochas.cs
@@ -1,11 +1,16 @@
-using System;
+//using System;
 using UnityEngine;
 
 public class Tochas : MonoBehaviour
 {
-    public GameObject _Fogo;
+    [SerializeField] private GameObject _Light;
+    [SerializeField] private GameObject _Fire;
     //public GameObject tochas;
-    public string _TagPlayer = "Player";
+    [SerializeField] private string _TagPlayer = "Player";
+
+    [SerializeField] private float _LightPower;
+    [SerializeField] private float _FirePower;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +21,15 @@ public class Tochas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_Light.activeSelf && _Light.transform.localScale.x < _LightPower)
+        {
+            ScaleProgressiveLight();
+        }
+
+        if (_Light.activeSelf && _Fire.transform.localScale.x < _FirePower)
+        {
+            ScaleProgressiveFire();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -24,7 +37,17 @@ public class Tochas : MonoBehaviour
         if (other.gameObject.CompareTag(_TagPlayer))
         {
             Debug.Log("Tocha acesa");
-            _Fogo.SetActive(true);
+            _Light.SetActive(true);
         }
+    }
+
+    public void ScaleProgressiveLight()
+    {
+        _Light.transform.localScale += new Vector3(0.04f, 0.02f, 0);
+    }
+
+    public void ScaleProgressiveFire()
+    {
+        _Fire.transform.localScale += new Vector3(0.002666f, 0.003999f, 0);
     }
 }
