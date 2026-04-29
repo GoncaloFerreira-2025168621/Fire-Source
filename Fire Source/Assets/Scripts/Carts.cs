92dd36a80b6e51cc43ed036b23d670063fc8a504
GoncaloFerreira-2025168621 <2025168621@estudantes.ips.pt>
2026-04-29 10:47:18 +0100

    cards

--- Fire Source/Assets/Scripts/Carts.cs
+++ Fire Source/Assets/Scripts/Carts.cs
@@ -7,23 +7,24 @@ public class Carts : MonoBehaviour
     [SerializeField] private int _explosionFireLv;
     [SerializeField] private int _flameThrowerAttackLv;
     [SerializeField] private int _fireBallsAttackLv;
-    [SerializeField] private int _card_melee = 1;
-    [SerializeField] private int _card_range = 2;
-    [SerializeField] private int _card_explosionFire = 3;
-    [SerializeField] private int _card_flameThrower = 4;
-    [SerializeField] private int _card_fireBalls = 5;
-    [SerializeField] private int _card_empty = 0;
+    [SerializeField] public int _card_melee = 1;
+    [SerializeField] public int _card_range = 2;
+    [SerializeField] public int _card_explosionFire = 3;
+    [SerializeField] public int _card_flameThrower = 4;
+    [SerializeField] public int _card_fireBalls = 5;
+    [SerializeField] public int _card_escolhida = 0;
     [SerializeField] public int _card_atual;
     [SerializeField] public int _Card_1;
     [SerializeField] public int _Card_2;
     [SerializeField] public int _Card_3;
+    [SerializeField] public int _Espaco_Cards_Disponivel = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _Card_1 = 0;
-        _Card_2 = 0;
-        _Card_3 = 0;
+        _Card_2 = 1;
+        _Card_3 = 2;
         _card_atual = _Card_1;
     }
 
@@ -35,41 +36,77 @@ public class Carts : MonoBehaviour
 
     public void MeleeAttackLv()
     {
-        _card_empty = _card_melee;
+        _card_escolhida = _card_melee;
         _meleeAttackLv++;
         if (_meleeAttackLv > 5)
         {
             _meleeAttackLv = 5; // Limita o nível máximo a 5
         }
+        _Espaco_Cards_Disponivel--;
 
     }
 
     public void RangeAttackLv()
     {
-
+        _card_escolhida = _card_range;
+        _rangeAttackLv++;
+        if (_rangeAttackLv > 5)
+        {
+            _rangeAttackLv = 5; // Limita o nível máximo a 5
+        }
     }
 
     public void ExplosionFireLv()
     {
+        _card_escolhida = _card_explosionFire;
+        _explosionFireLv++;
+        if (_explosionFireLv > 5)
+        {
+            _explosionFireLv = 5; // Limita o nível máximo a 5
+        }
     }
 
     public void FlameThrowerAttackLv()
     {
-
+        _card_escolhida = _card_flameThrower;
+        _flameThrowerAttackLv++;
+        if (_flameThrowerAttackLv > 5)
+        {
+            _flameThrowerAttackLv = 5; // Limita o nível máximo a 5
+        }
     }
 
     public void FireBallsAttackLv()
     {
-
-
+        _card_escolhida = _card_fireBalls;
+        _fireBallsAttackLv++;
+        if (_fireBallsAttackLv > 5)
+        {
+            _fireBallsAttackLv = 5; // Limita o nível máximo a 5
+        }
     }
 
     public void VerificationCard()
     {
-        if (_card_atual == _Card_1)
+        //Verifica se a cartas disponiveis (Card_1, Card_2, Card_3) sao diferentes da carta escolhida (_card_escolhida) e se a carta atual (_card_atual) é diferente da carta escolhida (_card_escolhida)
+        if (_card_atual != _card_escolhida)
         {
-            _card_atual = _Card_2;
-            _Card_1 = _card_empty;
+            if (_card_atual == _Card_1)
+            {
+                _card_atual = _Card_2;
+                _Card_1 = _card_escolhida;
+            }
+            else if (_card_atual == _Card_2 && _Card_1 != 0)
+            {
+                _card_atual = _Card_3;
+                _Card_2 = _card_escolhida;
+            }
+            else if (_card_atual == _Card_3 && _Card_1 != 0 && _Card_2 != 0)
+            {
+                _Card_3 = _card_escolhida;
+            }
         }
+
+
     }
 }
--- Fire Source/Assets/Scripts/Player Atttack.cs
+++ Fire Source/Assets/Scripts/Player Atttack.cs
@@ -3,10 +3,14 @@ using static UnityEngine.GraphicsBuffer;
 
 public class PlayerAttack : MonoBehaviour
 {
+
     [Header("General Attack Settings")]
     [SerializeField] private GameObject _PlayerDirection;
     [SerializeField] private float _AttackCooldown; // Tempo de cooldown do ataque em segundos
     [SerializeField] private Player_controller _movement;
+
+    [Header("Cards")]
+    [SerializeField] private Carts _cards;
 
     [Header("Melee Attack")]
     [SerializeField] private GameObject _MeleeAttack_lv1;
@@ -63,6 +67,26 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_cards._card_atual == _cards._card_melee) // Verifica se a carta atual é a de ataque corpo a corpo
+        {
+            _AttackCooldown = _MeleeAttackCooldown; // Define o cooldown do ataque para o valor do ataque corpo a corpo
+        }
+        else if (_cards._card_atual == _cards._card_range) // Verifica se a carta atual é a de ataque a distância
+        {
+            _AttackCooldown = _RangeAttackCooldown; // Define o cooldown do ataque para o valor do ataque a distância
+        }
+        else if (_cards._card_atual == _cards._card_explosionFire) // Verifica se a carta atual é a de ataque de explosão de fogo
+        {
+            _AttackCooldown = _ExplosionFireCooldown; // Define o cooldown do ataque para o valor do ataque de explosão de fogo
+        }
+        else if (_cards._card_atual == _cards._card_flameThrower) // Verifica se a carta atual é a de ataque de lança chamas
+        {
+            _AttackCooldown = _FlameThrowerCooldown; // Define o cooldown do ataque para o valor do ataque de lança chamas
+        }
+        else if (_cards._card_atual == _cards._card_fireBalls) // Verifica se a carta atual é a de ataque de bola de fogo
+        {
+            _AttackCooldown = _FireBallsCooldown; // Define o cooldown do ataque para o valor do ataque de bola de fogo
+        }
         _ExplosionFireTimer += Time.deltaTime; // Incrementa o cooldown do ataque de explosão de fogo a cada frame
         _RangeAttackTimer += Time.deltaTime; // Incrementa o cooldown do ataque a distância a cada frame
         _MeleeAttackTimer += Time.deltaTime; // Incrementa o cooldown do ataque corpo a corpo a cada frame
